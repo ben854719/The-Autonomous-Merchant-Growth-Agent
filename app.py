3058615f82126d84c7c7655e54f49c6f21c77192
Ben <185095765+ben854719@users.noreply.github.com>
2026-09-09 15:33:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from fastapi.responses import HTMLResponse, StreamingResponse
 import os
 import polars as pl
 import pandas as pd
-import matplotlib as plt
+import matplotlib
 matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 import base64
 import io
 from langchain_google_genai import ChatGoogleGenerativeAI
